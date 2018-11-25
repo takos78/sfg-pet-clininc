16f8a9203ef4c584ede3be46fad9264ff8549f57
takos78 <akos.tomasits@gmail.com>
2018-11-25 13:08:16 +0100

    Lists Vets on Vets page. Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -3,11 +3,14 @@ package guru.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class Pet extends BaseEntity {
 
-	private PetType prtType;
+	private PetType petType;
 	
 	private Owner owner;
 	
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetsController.java
@@ -1,15 +1,28 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.service.VetService;
 
 @Controller
 @RequestMapping("vet")
 public class VetsController {
 
+	private VetService vetService;
+	
+	@Autowired
+	public VetsController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+
+
 	@RequestMapping({"", "/", "index.html"})
 	public String start(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vet/index.html";
 	}
 	
