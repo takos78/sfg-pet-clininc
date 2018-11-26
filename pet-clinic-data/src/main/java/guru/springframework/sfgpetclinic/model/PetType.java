d9a045d904a4d4bc26d114b24bf464e78b0d44fc
takos78 <akos.tomasits@gmail.com>
2018-11-26 15:21:01 +0100

    PetTypes loaded to Map Store on Startup. Closes #24

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,5 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PetType extends BaseEntity {
 
+	private String name;
+	
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.service.OwnerService;
+import guru.springframework.sfgpetclinic.service.PetTypeService;
 import guru.springframework.sfgpetclinic.service.VetService;
 
 @Component
@@ -16,16 +18,29 @@ public class DataLoader implements CommandLineRunner {
 	
 	private final VetService vetService;
 	
+	private final PetTypeService petTypeService;
+	
 	
 	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
-
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		petTypeService.save(dog);
+
+		PetType cat = new PetType();
+		dog.setName("Cat");
+		petTypeService.save(cat);
+		
+		System.out.println("Loaded PetTypes ---");
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("John");
 		owner1.setLastName("Charmac");
