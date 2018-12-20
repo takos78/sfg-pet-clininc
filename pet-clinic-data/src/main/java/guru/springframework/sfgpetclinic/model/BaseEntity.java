343c35cf84cd42aaad737d931bd7a363098daba0
takos78 <akos.tomasits@gmail.com>
2018-12-20 18:21:44 +0100

    BaseEntity and Person turned into JPA base classes. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Data;
 
 @Data
+@MappedSuperclass
 public class BaseEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Long id; 
 	
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,14 +1,24 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@MappedSuperclass
 @Getter
 @Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class Person extends BaseEntity {
 
+	@Column(name = "first_name")
 	protected String firstName;
 	
+	@Column(name = "last_name")
 	protected String lastName;
 	
 }
