93ac19e37f9b23ee090db3ccd00e2ec1a26a8cf0
takos78 <akos.tomasits@gmail.com>
2018-12-20 21:51:17 +0100

    Added Visit entity. Closes #30

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,7 +18,7 @@ import lombok.ToString;
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
-@ToString(callSuper = true, exclude = {"owner"})
+@ToString(callSuper = true)
 public class Pet extends BaseEntity {
 
 	@Column
@@ -29,6 +33,12 @@ public class Pet extends BaseEntity {
 	
 	@ManyToOne
 	@JoinColumn(name = "owner_id")
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
 	private Owner owner;
 	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	private Set<Visit> visits = new HashSet<>();
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -2,17 +2,31 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@Entity
 @Getter
 @Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class Visit extends BaseEntity {
 
+	@Column
 	private LocalDate date;
 	
+	@Column
 	private String description;
 	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 	
 }
