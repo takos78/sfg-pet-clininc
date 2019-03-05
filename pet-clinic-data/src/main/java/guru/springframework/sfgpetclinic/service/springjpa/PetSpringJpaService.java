2c12e49d1de34b23711c93872dd2738824a9e5d9
takos78 <akos.tomasits@gmail.com>
2019-03-06 00:54:41 +0100

    #32: Pet Spring JPA Service added.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springjpa/PetSpringJpaService.java
@@ -0,0 +1,59 @@
+package guru.springframework.sfgpetclinic.service.springjpa;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repository.PetRepository;
+import guru.springframework.sfgpetclinic.service.PetService;
+
+public class PetSpringJpaService implements PetService {
+
+	private final PetRepository petRepository;
+	
+	
+	public PetSpringJpaService(PetRepository petRepository) {
+		this.petRepository = petRepository;
+	}
+	
+	
+	@Override
+	public Pet findById(Long id) {
+		return petRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Pet save(Pet entity) {
+		return petRepository.save(entity);
+	}
+
+	@Override
+	public Set<Pet> findAll() {
+		Set<Pet> pets = new HashSet<>();
+		petRepository.findAll().forEach(pets::add);
+		return pets;
+	}
+
+	@Override
+	public void delete(Pet entity) {
+		petRepository.delete(entity);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		petRepository.deleteById(id);
+	}
+
+	@Override
+	public Set<Pet> savePets(Set<Pet> pets) {
+		Set<Pet> savedPets = new HashSet<>();
+		pets.stream().forEach(pet -> {
+			Pet savedPet = this.save(pet);
+			savedPets.add(savedPet);
+		});
+		return savedPets;
+	}
+
+
+}
