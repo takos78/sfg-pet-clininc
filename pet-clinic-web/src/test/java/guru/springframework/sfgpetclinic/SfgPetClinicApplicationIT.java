0fe251444bedfd1a30aa10ed9b7a724d25991627
takos78 <akos.tomasits@gmail.com>
2019-03-20 21:15:53 +0100

    ApplicationTests renamed to ...IT

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationIT.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SfgPetClinicApplicationTests {
+public class SfgPetClinicApplicationIT {
 
 	@Test
 	public void contextLoads() {
